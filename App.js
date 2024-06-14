49133f07bb0e2aa4796eef450cb4ef1aa3454bae
Kashif <ahmed.kashif1001@gmail.com>
2024-06-14 14:15:04 +0500

    vector icon setup, navigation, tab navigation, bottom tab

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function App() {
+import { FontAwesome } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+        <Tab.Screen
+          name="HOME"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "HOME",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home-filled" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="REORDER"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "REORDER",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="reorder" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="CART"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "CART",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="shopping-cart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ACCOUNT"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "ACCOUNT",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-circle"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
