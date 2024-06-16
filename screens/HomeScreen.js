356d20b62b0857e29832054c104ad0876ac64ea4
Kashif <ahmed.kashif1001@gmail.com>
2024-06-16 23:50:19 +0500

    search box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,25 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "../components/Header";
+import { EvilIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   return (
 
-   
     // Background Linear Gradient
     <LinearGradient style={styles.container} colors={["#FDF0F3", "#FFFBFC"]}>
       <Header/>
+
+      <Text style={styles.textOne}>Match Your Style</Text>
+
+      {/* Search Box */}
+
+      <View style={styles.inputContainer}>
+      <AntDesign style={styles.icon} name="search1" size={24} color="#C0C0C0" />
+      <TextInput placeholder="Search" style={styles.searchBox}/>
+      </View>
     </LinearGradient>
   );
 }
@@ -17,25 +27,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // backgroundColor: 'orange',
   },
-  //   background: {
-  //     position: 'absolute',
-  //     left: 0,
-  //     right: 0,
-  //     top: 0,
-  //     height: 300,
-  //   },
-  //   button: {
-  //     padding: 15,
-  //     alignItems: 'center',
-  //     borderRadius: 5,
-  //   },
-  //   text: {
-  //     backgroundColor: 'transparent',
-  //     fontSize: 15,
-  //     color: '#fff',
-  //   },
+  textOne : {
+    fontSize: 28,
+    color: "#000000",
+    marginTop: 20
+  },
+  inputContainer :{
+    backgroundColor: '#FFFFFF',
+    height: 45,
+    borderRadius: 15,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: 20,
+    marginHorizontal: 20
+  },
+  icon : {
+    marginHorizontal: 5
+  },
+
+  searchBox : {
+    marginLeft:20,
+    fontSize:16
+    
+    // borderWidth: 1,
+    // borderColor: 'black'
+    }
+  
 });
