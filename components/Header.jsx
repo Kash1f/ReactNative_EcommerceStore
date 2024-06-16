69443294d96d2efaa3970c15523352ae632d35df
Kashif <ahmed.kashif1001@gmail.com>
2024-06-16 18:21:02 +0500

    reusable header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,62 +1,45 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Header = () => {
   return (
     <View style={styles.container}>
-
       <View style={styles.appIconContainer}>
-        <Image source={require("../assets/apps.png")} style={styles.appIcon}/>
-        
+        <Image source={require("../assets/apps.png")} style={styles.appIcon} />
       </View>
 
       <Image source={require("../assets/Ellipse2.png")} style={styles.profileIcon}/>
-
-
-
-
-      
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-
-
-container : {
- 
-  marginTop: 50,
-  flexDirection: 'row',
-  justifyContent: "space-between",
-  alignItems: 'center',
-  margin:10
-
+  container: {
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
   },
 
-appIconContainer : {
+  appIconContainer: {
     backgroundColor: "#FFFFFF",
-    height:44,
+    height: 44,
     width: 44,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 22,
-    
-
-},
-appIcon : {
-    height:28,
+  },
+  appIcon: {
+    height: 28,
     width: 28,
-    
-},
+  },
 
-profileIcon :{
-  height: 44,
-  width: 44,
-  borderRadius: 22,
- 
-}
-
-
-})
+  profileIcon: {
+    height: 44,
+    width: 44,
+    borderRadius: 22,
+  },
+});
